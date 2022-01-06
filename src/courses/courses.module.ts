0be7aaf1a8ed306dac3d15677c15ea391d261812
tinbui1908 <bhtt190800@gmail.com>
2022-01-06 18:27:52 +0700

    edit user, course module

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { Course } from 'src/courses/entity/course.entity';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [CoursesService],
-  controllers: [CoursesController]
+	imports: [SequelizeModule.forFeature([Course]), UsersModule],
+	providers: [CoursesService],
+	controllers: [CoursesController],
+	exports: [CoursesService]
 })
 export class CoursesModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { User } from './entity/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+	imports: [SequelizeModule.forFeature([User])],
+	controllers: [UsersController],
+	providers: [UsersService],
+	exports: [UsersService]
 })
 export class UsersModule {}
