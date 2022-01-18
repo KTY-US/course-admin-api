e47483580569455e3bf0712ba955d6f7ad4839c5
tinbui1908 <bhtt190800@gmail.com>
2022-01-18 08:15:03 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,21 @@ import 'core-js/proposals/reflect-metadata';
 async function bootstrap(): Promise<void> {
 	const port = process.env.PORT || 5001;
 	const app = await NestFactory.create(AppModule);
-	// const allowedOrigins = [
-	// 	'http://localhost:3000',
-	// 	'https://course-admin-app.netlify.app'
-	// ];
-	// app.enableCors({
-	// 	origin: function (origin, callback) {
-	// 		// allow requests with no origin
-	// 		// (like mobile apps or curl requests)
-	// 		if (!origin || allowedOrigins.indexOf(origin) === -1) {
-	// 			const msg = new Error(
-	// 				'The CORS policy for this site does not ' + 'allow access from the specified Origin.'
-	// 			);
-	// 			return callback(msg, false);
-	// 		}
-	// 		return callback(null, true);
-	// 	}
-	// });
-
-	app.enableCors({});
+	const allowedOrigins = ['http://localhost:3000', 'https://course-admin-app.netlify.app'];
+	app.enableCors({
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin || allowedOrigins.indexOf(origin) === -1) {
+				const msg = new Error(
+					'The CORS policy for this site does not ' + 'allow access from the specified Origin.'
+				);
+				return callback(msg, false);
+			}
+			return callback(null, true);
+		}
+	});
+	// app.enableCors({});
 	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(port);
 }
